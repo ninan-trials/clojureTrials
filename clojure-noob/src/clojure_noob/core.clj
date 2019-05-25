112266c4fc0140ec3ec01b73ac1370f8114dc45b
Ninan John <jninan@thoughtworks.com>
2019-05-25 12:39:29 +0530

    Create clojureTrials repo

---
+++ clojure-noob/src/clojure_noob/core.clj
@@ -0,0 +1,13 @@
+(ns clojure-noob.core
+  (:gen-class))
+
+(defn -main
+
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+ (println "Cleanliness is next to godliness")
+
+(defn train
+  []
+  (println "Choo choo!"))
